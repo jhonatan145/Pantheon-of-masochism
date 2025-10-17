9411f0c39d1d10f833ed9935699444465e7171a9
jhonatan145 <jhonnygg1208@gmail.com>
2025-10-16 23:44:24 -0600

    Delete app hollow directory

--- app hollow/js/script.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.getElementById('menu-btn');
-const menu = document.getElementById('menu');
-
-menuBtn.addEventListener('click', () => {
-    menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
-});
